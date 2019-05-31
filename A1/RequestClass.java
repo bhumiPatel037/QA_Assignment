43fb8ac37e01a3d38f74627ffd68b592596fad8a
bhumipatel <bh842792@dal.ca>
2019-05-31 12:53:29 -0300

    Commit file RequestClass

---
+++ A1/RequestClass.java
@@ -0,0 +1,15 @@
+public class RequestClass {
+
+    public String apikey;
+    public String username;
+    public String action;
+    public String drug;
+    public long quantity;
+    public String customer;
+    public String street;
+    public String city;
+    public String province;
+    public String country;
+    public String postalCode;
+
+}
