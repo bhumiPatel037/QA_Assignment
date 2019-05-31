37e32bf3e9c093160e69b3812183e07028fb2110
bhumipatel <bh842792@dal.ca>
2019-05-31 12:49:36 -0300

    Commit file IDatabase

--- A1/IDatabase.java
+++ A1/IDatabase.java
@@ -1,5 +1,13 @@
 // YOU ARE ALLOWED TO MODIFY THIS FILE
 public interface IDatabase
 {
+  //Return drugCount of valid drug
+  public Integer drugCount(String drugName);
+
+  // Return True= if drug is exist in system , false=if drug does not exist in system.
+  public Boolean isDrugExist(String drugName);
+
+  //return true= if valid drug has enough quantity , false=if drug count is insufficient
+  public boolean claimDrug(String drugName,Integer quantity);
 
 }
