0be2047ca09f31a5fb7eef2fa6db64cba1548bc7
bhumipatel <bh842792@dal.ca>
2019-07-19 13:06:40 -0300

    add solution for assignment2

---
+++ A4/Question2/Person.java
@@ -0,0 +1,44 @@
+public class Person
+{
+	private String name;
+
+	private String areaCode;
+	private String phoneNumber;
+
+     PersonAuthentication personAuthentication = new PersonAuthentication();
+
+	public Person(String name)
+	{
+		this.name = name;
+	}
+
+	public void setAreaCode(String areaCode)
+	{
+		this.areaCode = areaCode;
+	}
+	public String getAreaCode()
+	{
+		return areaCode;
+	}
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPhoneNumber()
+	{
+		if (areaCode != null && areaCode != "")
+		{
+			return "(" + areaCode + ") " + phoneNumber; 
+		}
+		return phoneNumber;
+	}
+
+	public void setLoginCredentials(String userName, String password)
+	{
+		personAuthentication.setLoginCredentials(userName,password);
+	}
+	public boolean authenticateUser()
+	{
+		return personAuthentication.authenticateUser();
+	}
+}
